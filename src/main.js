dc9bffb5ee037e816ec24fdde8a2b0b55c9b599d
RaphaelDidjurgeit <didjurgeit.raphael@web.de>
2022-01-17 12:36:32 +0100

    change bootstrap implementation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/src/jquery.js'
+import 'popper.js/dist/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faPlus,
